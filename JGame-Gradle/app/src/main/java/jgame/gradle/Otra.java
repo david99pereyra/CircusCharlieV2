46a4819e56cf7761e7a1268e921f6463f224784e
david99pereyra <david99pereyra@gmail.com>
2024-04-29 20:04:25 -0300

    Cargando elproyecto dado por la catedra de Programacion Orientada a Objetos

---
+++ JGame-Gradle/app/src/main/java/jgame/gradle/Otra.java
@@ -0,0 +1,10 @@
+package jgame.gradle;
+
+
+///gradle run -PclassToExecute=jgame.gradle.Otra
+
+public class Otra{
+	public static void main(String...args){
+		System.out.println("Hola Mundo!!! desde Otra");
+	}
+}
---
+++ JGame-Gradle/app/src/main/java/jgame/gradle/Personaje.java
@@ -0,0 +1,50 @@
+package jgame.gradle;
+
+import java.awt.*;
+import java.awt.event.*; //eventos
+
+import java.awt.image.*;  //imagenes
+import javax.imageio.*; //imagenes
+
+import java.awt.geom.*; //Point2d
+
+public class Personaje{
+
+    BufferedImage imagen=null;
+    private Point2D.Double posicion  = new Point2D.Double();
+
+    public Personaje(){
+
+    }
+
+    public void setImagen(BufferedImage img){
+        this.imagen=img;
+
+    }
+
+    public void setPosicion(double x, double y){
+        posicion.setLocation(x, y);
+    }
+
+    public void setX(double x){
+        posicion.x=x;
+    }
+
+    public void setY(double y){
+        posicion.y=y;
+    }
+    public double getX(){
+        return posicion.getX(); 
+    }
+
+    public double getY(){
+        return posicion.getY(); 
+    }
+    public void update(double delta){
+
+    }
+
+    public void draw(Graphics2D g){
+        g.drawImage(imagen,(int)posicion.getX(),(int)posicion.getY(),null);
+    }
+}
