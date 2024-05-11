7cc44128ddf809dbe1aaf6bd4d88eddb8c72efc1
david99pereyra <david99pereyra@gmail.com>
2024-05-11 15:20:48 -0300

    Clase score, encargada de dibujar la grafica del score, bonus, hi-score, nivel actual, faltaria que se dibuje las vidas

--- app/src/main/java/jgame/gradle/CircusCharlie/Score.java
+++ app/src/main/java/jgame/gradle/CircusCharlie/Score.java
@@ -3,16 +3,19 @@ package jgame.gradle.CircusCharlie;
 import java.awt.image.*;
 import java.util.Timer;
 import java.util.TimerTask;
+
+import jgame.gradle.FontManager;
+
 import java.awt.*;
 
 public class Score extends ObjetoGrafico {
-
 
     private int bonus = 5000;
     private int score = 0;
     private int hiScore = 0;
     private int state = 0;
 
+    private boolean descuentoBonusActivo = true;
 
     public Score() {
         this.setPosition(10, 10);
@@ -20,14 +23,15 @@ public class Score extends ObjetoGrafico {
     }
 
     public void update() {
+        FontManager.getInstance();
 
         BufferedImage img = new BufferedImage(800, 300, BufferedImage.TYPE_INT_ARGB);
         Graphics g = img.createGraphics();
 
         g.setColor(Color.WHITE);
-        g.setFont(new Font("Arial", Font.BOLD,20));
+        g.setFont(new Font("Pixel Emulator", Font.BOLD, 20));
 
-        String scoreText = String.format("%06d",score);
+        String scoreText = String.format("%06d", score);
         g.drawString(scoreText, 200, 55);
 
         String HIText = "HI-" + String.format("%06d", hiScore);
@@ -49,14 +53,38 @@ public class Score extends ObjetoGrafico {
 
     }
 
+    public void detenerDescuentoBonus() {
+        descuentoBonusActivo = false;
+        sumarRestoBonusAScore();
+    }
+
+    private synchronized void sumarRestoBonusAScore() {
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                while (bonus > 0) {
+                    score++;
+                    bonus--;
+                    update();
+                }
+                
+            }
+        }, 1000,6000);
+         // Actualizar la representación visual
+    }
+    
+
     private void iniciarTemporizador() {
         Timer timer = new Timer();
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
-                disminuirBonus();
+                if(descuentoBonusActivo){
+                    disminuirBonus();
+                }
             }
-        }, 200, 200); // Inicia el temporizador después de 200 ms, y luego lo repite cada 200 ms
+        }, 1000, 300); // Inicia el temporizador después de 200 ms, y luego lo repite cada 200 ms
     }
 
     private synchronized void disminuirBonus() {
@@ -69,13 +97,12 @@ public class Score extends ObjetoGrafico {
         }
     }
 
-    public void nivelActual(int nivel){
-        state += nivel;
+    public void nivelActual(int nivel) {
+        this.state += nivel;
     }
 
-    public void sumarScore(int valor){
-        score += valor;
+    public void sumarScore(int valor) {
+        this.score += valor;
     }
 
-    
 }
