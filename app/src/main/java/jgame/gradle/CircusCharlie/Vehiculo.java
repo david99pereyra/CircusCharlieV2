301e3556b6ac3040b1f2165b02fa6e4637965dfb
felisantillan00 <130380077+felisantillan00@users.noreply.github.com>
2024-06-10 09:11:51 -0300

    Cambios relevantes en el leon y vehiculo.

--- app/src/main/java/jgame/gradle/CircusCharlie/Vehiculo.java
+++ app/src/main/java/jgame/gradle/CircusCharlie/Vehiculo.java
@@ -1,40 +1,24 @@
 package jgame.gradle.CircusCharlie;
-
-import java.awt.*;
+import jgame.gradle.ObjetoGrafico;
 import java.awt.image.*;
 import javax.imageio.*;
-
-import jgame.gradle.ObjetoGrafico;
-
 import java.io.*;
 import java.util.*;
 
-public abstract class Vehiculo extends ObjetoGrafico{
-	private double positionX = 0;
-	private double positionY = 0;
-    private double PISO;
-    // private int velocityX = 0;
-    private int direccionAngulo = 0;   
-	private int indiceImagenActual = 0;
-	private int idx = 0;
-    private BufferedImage imagen = null;
+public abstract class Vehiculo extends ObjetoGrafico implements ObjetoMovible{
+    protected double PISO;
+    protected int direccionAngulo = 0;
+	protected int idx = 0;
+	protected int indiceImagenActual = 0;
 
     public Vehiculo(String filename) {
 		super(filename);
 		try {
 			imagen= ImageIO.read(getClass().getClassLoader().getResourceAsStream(filename));
-		} catch (IOException e) {
+		} catch(IOException e) {
 			System.out.println("ZAS! en ObjectoGrafico "+e);
 		}
     }
-    public void setPosition(double d,double e){
-		this.positionX = d;
-		this.positionY = e;
-	}
-	//No utilizamos este ya que necesitamos para el swap
-	public void display(Graphics2D g2) {
-		g2.drawImage(imagen,(int) this.positionX,(int) this.positionY,null);
-	}
 
     public void left(double velX) {
 		positionX -= velX;
@@ -44,30 +28,6 @@ public abstract class Vehiculo extends ObjetoGrafico{
 	public void right(double velX) {
 		positionX += velX;
 		direccionAngulo= 1;
-	}
-
-    public int getWidth(){
-		return imagen.getWidth();
-	}
-	
-	public int getHeight(){
-		return imagen.getHeight();
-	}
-
-	public double getX(){
-		return positionX;
-	}
-
-	public double getY(){
-		return positionY;
-	}
-
-	public void setX(double x){
-		this.positionX = x;
-	}
-
-	public void setY(double y){
-		this.positionY = y;
 	}
 
     public void setImagen(String img){
